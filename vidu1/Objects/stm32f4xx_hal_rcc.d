ec3ea14746e7d518b28f91684338c42aa80dbb2e
Tan Tram <tan.tramminh99@gmail.com>
2025-10-08 23:49:30 +0700

    Update led works

---
+++ vidu1/Objects/stm32f4xx_hal_rcc.d
@@ -0,0 +1,29 @@
+./objects/stm32f4xx_hal_rcc.o: src\stm32f4xx_hal_rcc.c \
+  inc\stm32f4xx_hal.h inc\stm32f4xx_hal_conf.h inc\stm32f4xx_hal_rcc.h \
+  inc\stm32f4xx_hal_def.h inc\stm32f4xx.h inc\stm32f411xe.h \
+  C:\Keil_v5\ARM\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
+  inc\system_stm32f4xx.h inc\Legacy\stm32_hal_legacy.h \
+  inc\stm32f4xx_hal_rcc_ex.h inc\stm32f4xx_hal_exti.h \
+  inc\stm32f4xx_hal_gpio.h inc\stm32f4xx_hal_gpio_ex.h \
+  inc\stm32f4xx_hal_dma.h inc\stm32f4xx_hal_dma_ex.h \
+  inc\stm32f4xx_hal_cortex.h inc\stm32f4xx_hal_adc.h \
+  inc\stm32f4xx_ll_adc.h inc\stm32f4xx_hal_adc_ex.h \
+  inc\stm32f4xx_hal_can.h inc\stm32f4xx_hal_crc.h \
+  inc\stm32f4xx_hal_cryp.h inc\stm32f4xx_hal_dac.h \
+  inc\stm32f4xx_hal_dcmi.h inc\stm32f4xx_hal_eth.h \
+  inc\stm32f4xx_hal_flash.h inc\stm32f4xx_hal_flash_ex.h \
+  inc\stm32f4xx_hal_flash_ramfunc.h inc\stm32f4xx_hal_sram.h \
+  inc\stm32f4xx_hal_nor.h inc\stm32f4xx_hal_nand.h \
+  inc\stm32f4xx_hal_pccard.h inc\stm32f4xx_hal_hash.h \
+  inc\stm32f4xx_hal_i2c.h inc\stm32f4xx_hal_i2c_ex.h \
+  inc\stm32f4xx_hal_i2s.h inc\stm32f4xx_hal_i2s_ex.h \
+  inc\stm32f4xx_hal_iwdg.h inc\stm32f4xx_hal_pwr.h \
+  inc\stm32f4xx_hal_pwr_ex.h inc\stm32f4xx_hal_rng.h \
+  inc\stm32f4xx_hal_rtc.h inc\stm32f4xx_hal_rtc_ex.h \
+  inc\stm32f4xx_hal_sd.h inc\stm32f4xx_ll_sdmmc.h \
+  inc\stm32f4xx_hal_spi.h inc\stm32f4xx_hal_tim.h \
+  inc\stm32f4xx_hal_tim_ex.h inc\stm32f4xx_hal_uart.h \
+  inc\stm32f4xx_hal_usart.h inc\stm32f4xx_hal_irda.h \
+  inc\stm32f4xx_hal_smartcard.h inc\stm32f4xx_hal_wwdg.h \
+  inc\stm32f4xx_hal_pcd.h inc\stm32f4xx_ll_usb.h \
+  inc\stm32f4xx_hal_pcd_ex.h inc\stm32f4xx_hal_hcd.h
